cfc090453578c22695a2242778717429d71b95ba
haniffm <haniffm@kth.se>
2019-04-10 16:42:07 +0200

    Install requirements for django tests

--- setup.py
+++ setup.py
@@ -28,11 +28,11 @@ setup(
     license='BSD',
     packages=['sql_server', 'sql_server.pyodbc'],
     install_requires=[
-        'Django>=1.11.15,<2.0',
+        'Django>=1.11,<1.12',
         'pyodbc>=3.0',
     ],
     extras_require={
-        'tests': ['dj-database-url==0.5.0', 'tblib==1.3.2'],
+        'tests': ['dj-database-url==0.5.0'],
     },
     classifiers=CLASSIFIERS,
     keywords='azure django',
